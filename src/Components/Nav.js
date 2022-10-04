7cab2ba9df8976d3eed211127db325d70294dd4e
Nereida Rondon <Nereida.Rondon5@gmail.com>
2022-10-04 18:41:56 -0400

    Trying out Link

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import '../components.css';
 import cloud from '../images/Cloud.png';
@@ -10,15 +11,15 @@ export default function NavBar(){
             <Nav as='h4'  className='mt-0 pt-5' variant="tabs" defaultActiveKey="/home">
 
                 <Nav.Item className=''>
-                    <Nav.Link as={Link} to={href='https://nereidarondon.github.io/Weather/'}>Home</Nav.Link>
+                    <Nav.Link as={Link} to={'https://nereidarondon.github.io/Weather/'}>Home</Nav.Link>
                 </Nav.Item>
 
                 <Nav.Item>
-                    <Nav.Link as={Link} to={href='https://nereidarondon.github.io/Weather/weatherdashboard'}>Dashboard</Nav.Link>
+                    <Nav.Link as={Link} to={'https://nereidarondon.github.io/Weather/weatherdashboard'}>Dashboard</Nav.Link>
                 </Nav.Item>
 
                 <Nav.Item>
-                    <Nav.Link as={Link} to={href='https://nereidarondon.github.io/Weather/map'}>Map</Nav.Link>
+                    <Nav.Link as={Link} to={'https://nereidarondon.github.io/Weather/map'}>Map</Nav.Link>
                 </Nav.Item>
 
                 <img className='cloud' src={cloud} alt='sun and cloud icon'/>

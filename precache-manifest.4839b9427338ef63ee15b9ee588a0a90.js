34d69de7cf4189914845876fc877a63832adf108
Nereida Rondon <Nereida.Rondon5@gmail.com>
2022-10-04 15:49:53 -0400

    Updates

---
+++ precache-manifest.4839b9427338ef63ee15b9ee588a0a90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5327a51e5f8440a9989f",
+    "url": "/Weather/static/css/main.ca75e827.chunk.css"
+  },
+  {
+    "revision": "5327a51e5f8440a9989f",
+    "url": "/Weather/static/js/main.e068e245.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "97b6e8757eb024f2cb9c",
+    "url": "/Weather/static/css/2.3a70578e.chunk.css"
+  },
+  {
+    "revision": "97b6e8757eb024f2cb9c",
+    "url": "/Weather/static/js/2.7b603576.chunk.js"
+  },
+  {
+    "revision": "a55a84b7e8ff2df821606ebf83b15a3d",
+    "url": "/Weather/static/media/refresh.a55a84b7.svg"
+  },
+  {
+    "revision": "979a336c61a3e5827e136e34d25b102c",
+    "url": "/Weather/static/media/x.979a336c.svg"
+  },
+  {
+    "revision": "6496866f16d66c06e5358ec68de2ca81",
+    "url": "/Weather/static/media/Cloud.6496866f.png"
+  },
+  {
+    "revision": "26d51d63f784aab4ae93e52f79af9824",
+    "url": "/Weather/index.html"
+  }
+];
